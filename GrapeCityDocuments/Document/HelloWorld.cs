c2a9af95b690d35f3498cdba8aeaae62234f24cf
Xu, Changhui <changhuixu@hotmail.com>
2018-06-11 15:12:02 -0500

    add grape city documents examples

---
+++ GrapeCityDocuments/Document/HelloWorld.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using GrapeCity.Documents.Pdf;
+using GrapeCity.Documents.Text;
+
+namespace GrapeCityDocuments.Document
+{
+    public class HelloWorld
+    {
+        public static void Generate()
+        {
+            GcPdfDocument doc = new GcPdfDocument();
+            // Add a page, get its graphics:
+            GcPdfGraphics g = doc.NewPage().Graphics;
+            // Render a string into the page:
+            g.DrawString("Hello, World!",
+                // Use a standard font (the 14 standard PDF fonts are built into GcPdf
+                // and are always available):
+                new TextFormat() { Font = StandardFonts.Times, FontSize = 12 },
+                // GcPdf page coordinates start at top left corner, using 72 dpi by default:
+                new PointF(72, 72));
+            // Save the PDF:
+            doc.Save("HelloWorld.pdf");
+        }
+    }
+}
---
+++ GrapeCityDocuments/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using GrapeCityDocuments.Document;
+
+namespace GrapeCityDocuments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //HelloWorld.Generate();
+            //DocProperties.Create("docProperties.pdf");
+            //LinearizedPdf.Create("linearized.pdf");
+            PdfPageSize.Create("pdfPageSize.pdf");
+        }
+    }
+}
